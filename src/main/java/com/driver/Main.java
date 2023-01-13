98058ba6e4e76bf46a4a5a10fb1d73505d9aeb3e
Pramila Walke <walkepramila@gmail.com>
2023-01-13 14:41:11 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rw=new RWOnly();
+        //rw.name="pramila";
+       // System.out.println(rw.name);
+        //can not access directly private data
+        rw.setName("pramila");
+        System.out.println(rw.getName());
+
+    }
   
 }
